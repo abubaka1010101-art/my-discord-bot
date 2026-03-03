897a2067738aae0a868f107134decef4e93937c5
abubaka1010101-art <abubaka1010101@gmail.com>
2026-03-04 01:50:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot is online as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+
+  // Ignore bots
+  if (message.author.bot) return;
+
+  // Ignore DMs (only work in servers)
+  if (!message.guild) return;
+
+  const yourID = '1238835145415462963';
+
+  if (message.mentions.users.has(yourID)) {
+    message.channel.send("Stop pinging my owner!");
+  }
+
+});
+
+client.login('client.login(process.env.TOKEN);');
